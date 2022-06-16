d83dc5feda6dfaf210aeca9f42702f9f7f7d1d30
shaypexe <83253013+shaypexe@users.noreply.github.com>
2022-06-16 14:21:49 +1000

    Created New Avatar Prefab to save the program
    Created avatar ragdoll using cubes and objects then added old script onto them to get it to work

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Game : MonoBehaviour
-{
+{   //
     public GameObject Player1;
+    //is
     public GameObject Player2;
     public Text PlayerOneScoreText;
     public int PlayerOneScore;
@@ -13,24 +14,20 @@ public class Game : MonoBehaviour
     public int PlayerTwoScore;
     public GameObject SpawnOne;
     public GameObject SpawnTwo;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  
 
-    // Update is called once per frame
+     
     public void Respawn(int Playernum)
-    {
+    {   //
         Player1.transform.position = SpawnOne.transform.position;
         Player2.transform.position = SpawnTwo.transform.position;
-
+        //
         if(Playernum == 1)
         {
             PlayerTwoScore++;
             PlayerTwoScoreText.text = PlayerTwoScore.ToString();
 
-        }
+        }//
         else
         { 
             PlayerOneScore++;
